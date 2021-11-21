ac1720130ccfa11f53b857ee6db2b1885c99cac1
psmigielski <walek.smigielski@gmail.com>
2021-11-22 00:17:07 +0100

    remove not neccesary packages

--- api/src/routers/Auth.router.ts
+++ api/src/routers/Auth.router.ts
@@ -6,8 +6,8 @@ import schemaValidator from "../middleware/schemaValidator";
 const authRouter = express.Router();
 
 const authController = new AuthController();
-authRouter.post("/register", schemaValidator("/../schemas/register.schema.json"), authController.register.bind(authController));
-authRouter.post("/login", schemaValidator("/../schemas/login.schema.json"), authController.login.bind(authController));
+authRouter.post("/register", schemaValidator("/../../schemas/register.schema.json"), authController.register.bind(authController));
+authRouter.post("/login", schemaValidator("/../../schemas/login.schema.json"), authController.login.bind(authController));
 authRouter.post("/logout", checkJwt, authController.logout.bind(authController));
 authRouter.get("/verify/:id", authController.verify.bind(authController));
 
