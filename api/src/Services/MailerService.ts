e1908b28c1c72389a69293c231b94e9477c2ae68
psmigielski <walek.smigielski@gmail.com>
2021-12-26 14:26:09 +0100

    move mails to mailerservice

--- api/src/Services/MailerService.ts
+++ api/src/Services/MailerService.ts
@@ -13,6 +13,26 @@ class MailerService {
         });
         let info = await transporter.sendMail(mailContent);
     }
+    public static sendVerificationMail(email: string, id: string){
+        MailerService.sendMail({
+            from: "Daily Routine",
+            to: email,
+            subject: "Verify your account",
+            html: `<h1>Hi!</h1>
+            <p> To verify your email, please visit the following <a href="http://localhost:4000/v1/api/auth/verify/${id}" >link</a></p>
+            <br><p> Cheers! </p>`,
+        });
+    }
+    public static sendResetRequest(email:string, id: string){
+        MailerService.sendMail({
+            from: "Daily Routine",
+            to: email,
+            subject: "Reset your password",
+            html: `<h1>Hi!</h1>
+            <p> To reset your password, please visit the following <a href="http://localhost:4000/v1/api/auth/reset/${id}" >link</a></p>
+            <br><p> Cheers! </p>`,
+        });
+    }
 }
 
 export default MailerService
