0e4fe8165ceaef6e2df5227ef436142e698ec55d
psmigielski <walek.smigielski@gmail.com>
2021-12-26 20:15:14 +0100

    add better error messages for schema validator

--- api/src/middleware/schemaValidator.ts
+++ api/src/middleware/schemaValidator.ts
@@ -4,6 +4,7 @@ import addFormats from "ajv-formats";
 import fs from "fs";
 import IUser from "../types/IUser";
 import ApiErrorException from "../Exceptions/ApiErrorException";
+import validation from "ajv/dist/vocabularies/validation";
 
 const schemaValidator = (pathToSchema: string) => {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -12,8 +13,31 @@ const schemaValidator = (pathToSchema: string) => {
         const schema: JSONSchemaType<IUser> = JSON.parse(fs.readFileSync(`${__dirname}${pathToSchema}`).toString());
         const validate: ValidateFunction = ajv.compile(schema)
         if (!validate(req.body)) {
+            console.log(validate.errors);
             if (validate.errors !== undefined && validate.errors !== null) {
-                throw new ApiErrorException(`${validate.errors[0].instancePath.substring(1)} ${validate.errors[0].message}`, 400);
+                switch(validate.errors[0].keyword){
+                    case "pattern":
+                        throw new ApiErrorException(`${validate.errors[0].instancePath.substring(1)} does not match pattern`, 400);
+                    break;
+                    case "type":
+                        throw new ApiErrorException(`${validate.errors[0].instancePath.substring(1)} has invalid type`, 400);
+                    break;
+                    case "required":
+                        throw new ApiErrorException(`${validate.errors[0].params.missingProperty} required`, 400);
+                    break;
+                    case "additionalProperties":
+                        throw new ApiErrorException(`no additional properties allowed`, 400);
+                    break;
+                    case "maxLength":
+                        throw new ApiErrorException(`${validate.errors[0].instancePath.substring(1)} is too long`, 400);
+                    break;
+                    case "minLength":
+                        throw new ApiErrorException(`${validate.errors[0].instancePath.substring(1)} is too short`, 400);
+                    break;
+                    case "format":
+                        throw new ApiErrorException(`must match ${validate.errors[0].params.format} format`, 400);
+                    break;
+                }
             }
         }
         next();
